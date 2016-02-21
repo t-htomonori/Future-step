74304bdb3691a311960fa61b4a96e62cac187629
hori <hori@example.com>
2016-02-21 07:54:59 +0000

    commit_1

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+  def top
+  end
+
+  def programmer
+  end
+
+  def consulting
+  end
+
+  def management
+  end
+
+  def teacher  	
+  end
+
+  def cafe
+  end
+
+  def job
+  end
+end
